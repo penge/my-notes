caa3967610b66fb4f97081984cd7d8c66d3e9617
bbb651 <53972231+bbb651@users.noreply.github.com>
2019-11-17 22:41:48 +0200

    Compacts the code
    Compacts the code by removing redundant code and changing the anonymous functions to arrow functions.

--- notes.js
+++ notes.js
@@ -1,16 +1,10 @@
 /* global chrome */
 
-var initialized = false;
-var textarea = document.getElementById("textarea");
+const textarea = document.getElementById("textarea");
 
-chrome.storage.sync.get(["newtab"], function (result) {
-  textarea.value = result.newtab || '';
-  initialized = true;
-});
-
-textarea.addEventListener("input", function () {
-  if (!initialized) {
-    return;
-  }
-  chrome.storage.sync.set({ newtab: textarea.value });
+chrome.storage.sync.get(["newtab"], result => {
+  textarea.value = result.newtab || "";
+  textarea.addEventListener("input", () => {
+    chrome.storage.sync.set({ newtab: textarea.value });
+  });
 });
