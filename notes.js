b7ed5144a0dae579e380c6a167a076c7d5e5d1c3
Viljami Vastamäki <viljami.vastamaki@nordcloud.com>
2019-11-18 20:56:35 +0200

    Keep text blurred until click

--- notes.js
+++ notes.js
@@ -4,6 +4,9 @@ const textarea = document.getElementById("textarea");
 
 chrome.storage.sync.get(["newtab"], result => {
   textarea.value = result.newtab || "";
+  textarea.addEventListener("click", () => {
+document.getElementById("textarea").classList.remove('blur');
+})
   textarea.addEventListener("input", () => {
     chrome.storage.sync.set({ newtab: textarea.value });
   });
