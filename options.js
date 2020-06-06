74636d3e24f721b47ab537d00eb84fdc96aae3d5
penge <ppenge@gmail.com>
2020-06-06 23:48:40 +0200

    Add custom theme events

--- options.js
+++ options.js
@@ -12,6 +12,7 @@ import {
   setFont,
   setSize,
   setTheme,
+  setCustomTheme,
   setFocus,
   setNewtab,
   setTab,
@@ -21,11 +22,12 @@ import {
   setVersion,
 } from "./options/state.js";
 
-chrome.storage.local.get(["font", "size", "theme", "focus", "newtab", "tab", "sync"], local => {
-  const { font, size, theme, focus, newtab, tab, sync } = local;
+chrome.storage.local.get(["font", "size", "theme", "customTheme", "focus", "newtab", "tab", "sync"], local => {
+  const { font, size, theme, customTheme, focus, newtab, tab, sync } = local;
   setFont(font);
   setSize(size);
   setTheme(theme);
+  setCustomTheme(customTheme);
   setFocus(focus);
   setNewtab(newtab);
   setTab(tab);
@@ -38,6 +40,7 @@ chrome.storage.onChanged.addListener((changes, areaName) => {
     setChange(changes["font"], setFont);
     setChange(changes["size"], setSize);
     setChange(changes["theme"], setTheme);
+    setChange(changes["customTheme"], setCustomTheme);
     setChange(changes["focus"], setFocus);
     setChange(changes["newtab"], setNewtab);
     setChange(changes["tab"], setTab);
