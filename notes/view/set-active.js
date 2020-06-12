9440fd417ab6f2df052f52ad12065329a59c5435
penge <ppenge@gmail.com>
2020-06-12 14:16:44 +0200

    Autofocus the note
    This was removed in the past as it looked like the cause of not having the address bar selected on CTRL + T, but it was actually an issue with non-persistent background script getting started (from Innactive to Active) and then using internal chrome.tabs.update. That problem occured only in OSX and Windows. In Chrome OS, it all worked well.
    
    Adding autofocus back, with a better implementation this time, using selection and range.

--- notes/view/set-active.js
+++ notes/view/set-active.js
@@ -1,8 +1,21 @@
-/* global document */
+/* global window, document, setTimeout */
 
 import { noteName, noteOptions, content } from "./elements.js";
 import { isReserved } from "../reserved.js";
 import attachOptions from "./attach-options.js";
+
+export const autofocus = () => {
+  setTimeout(() => {
+    const selection = window.getSelection();
+
+    const range = document.createRange();
+    range.setStart(content, 0);
+    range.setEnd(content, 0);
+
+    selection.removeAllRanges();
+    selection.addRange(range);
+  });
+};
 
 export default function setActive(name, html, { renameNote, deleteNote }) {
   document.title = name;
@@ -11,6 +24,7 @@ export default function setActive(name, html, { renameNote, deleteNote }) {
   noteName.classList.toggle("reserved", isReserved(name));
 
   content.innerHTML = html;
+  autofocus();
 
   attachOptions(name, { noteOptions, renameNote, deleteNote });
 }
