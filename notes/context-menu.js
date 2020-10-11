5621d40c6da5d2735a24c43555e212ff62b973a3
penge <ppenge@gmail.com>
2020-10-11 13:52:49 +0200

    Don't show context menu for Clipboard
    - Clipboard cannot be renamed or deleted at the moment

--- notes/context-menu.js
+++ notes/context-menu.js
@@ -5,6 +5,7 @@ import range from "./range.js";
 
 import { contextMenu, renameAction, deleteAction } from "./view/elements.js";
 import { renameNoteModal, deleteNoteModal } from "./modals.js";
+import { isReserved } from "./reserved.js";
 
 const hide = () => {
   if (!document.body.classList.contains("with-context-menu")) {
@@ -15,6 +16,10 @@ const hide = () => {
 };
 
 const show = (x, y, noteName) => {
+  if (isReserved(noteName)) {
+    return;
+  }
+
   range.save();
 
   contextMenu.style.left = x + "px";
