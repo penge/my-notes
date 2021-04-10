a49d6ec95309ad5ff87bbfb6427fd3777e3277c1
penge <ppenge@gmail.com>
2021-04-10 16:22:17 +0200

    Add Synced to Info tooltip

--- src/notes/components/NoteInfo.tsx
+++ src/notes/components/NoteInfo.tsx
@@ -10,6 +10,7 @@ interface NoteInfoProps {
 const NoteInfo = ({ note }: NoteInfoProps): h.JSX.Element => {
   const created = parseDate(note.createdTime);
   const modified = parseDate(note.modifiedTime);
+  const synced = note.sync && parseDate(note.sync.file.modifiedTime);
 
   const text = note.content.replace(/<[^>]*>/g, " ").replace(/\s+/g, " ").trim();
   const wordCount = text.length ? text.split(" ").length : 0;
@@ -22,6 +23,7 @@ const NoteInfo = ({ note }: NoteInfoProps): h.JSX.Element => {
       <div className="column">
         <div>Created</div>
         <div>Modified</div>
+        {synced && <div>Synced</div>}
 
         <div>&nbsp;</div>
         <div>Words</div>
@@ -34,16 +36,19 @@ const NoteInfo = ({ note }: NoteInfoProps): h.JSX.Element => {
           <div className="column">
             <span className="year">{created?.year}</span>
             <span className="year">{modified?.year}</span>
+            {synced && <span className="year">{synced.year}</span>}
           </div>
 
           <div className="column">
             <span className="month-and-day">{`${created?.month} ${created?.day}`}</span>
             <span className="month-and-day">{`${modified?.month} ${modified?.day}`}</span>
+            {synced && <span className="month-and-day">{`${synced?.month} ${synced?.day}`}</span>}
           </div>
 
           <div className="column">
             <span className="time">{created?.time}</span>
             <span className="time">{modified?.time}</span>
+            {synced && <span className="time">{synced?.time}</span>}
           </div>
         </div>
 
