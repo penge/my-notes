aa47919febaf6d200b6ecf4157aac462e79f5d14
penge <ppenge@gmail.com>
2022-05-25 00:20:30 +0200

    Sort Command palette notes same as in Sidebar

--- src/notes/sort/index.ts
+++ src/notes/sort/index.ts
@@ -2,17 +2,35 @@ import { SidebarNote } from "notes/adapters";
 import { NotesOrder } from "shared/storage/schema";
 
 export const sortNotes = (notes: SidebarNote[], notesOrder: NotesOrder, custom?: string[]): SidebarNote[] => {
+  const pinnedNotes = notes.filter((note) => note.pinnedTime);
+  const unpinnedNotes = notes.filter((note) => !note.pinnedTime);
+
   if (notesOrder === NotesOrder.Alphabetical) {
-    return notes.sort((a, b) => a.name.localeCompare(b.name));
+    const alphabeticalCompare = (a: SidebarNote, b: SidebarNote) => a.name.localeCompare(b.name);
+    return [
+      ...pinnedNotes.sort(alphabeticalCompare),
+      ...unpinnedNotes.sort(alphabeticalCompare),
+    ];
   }
 
   if (notesOrder === NotesOrder.NewestFirst) {
-    return notes.sort((a, b) => -a.modifiedTime.localeCompare(b.modifiedTime));
+    const newestFirstCompare = (a: SidebarNote, b: SidebarNote) => -a.modifiedTime.localeCompare(b.modifiedTime);
+    return [
+      ...pinnedNotes.sort(newestFirstCompare),
+      ...unpinnedNotes.sort(newestFirstCompare),
+    ];
   }
 
   if (custom?.length) {
-    return notes.sort((a, b) => custom.indexOf(a.name) - custom.indexOf(b.name));
+    const customCompare = (a: SidebarNote, b: SidebarNote) => custom.indexOf(a.name) - custom.indexOf(b.name);
+    return [
+      ...pinnedNotes.sort(customCompare),
+      ...unpinnedNotes.sort(customCompare),
+    ];
   }
 
-  return notes;
+  return [
+    ...pinnedNotes,
+    ...unpinnedNotes,
+  ];
 };
