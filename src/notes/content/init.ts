3fe2b7e8d6a51526feae74802897a66529209a5d
penge <ppenge@gmail.com>
2023-12-20 23:37:27 +0100

    Add ability to resize imgs
    - hold Ctrl (or Cmd), move mouse over the image, move mouse wheel up and down to resize the image
    - double click on the image to restore its original size

--- src/notes/content/init.ts
+++ src/notes/content/init.ts
@@ -1,7 +1,9 @@
+import { initImgs } from "./img";
 import { initTables } from "./table";
 import renderTableContextMenu from "./render-table-context-menu";
 
 export default () => {
+  initImgs();
   initTables({
     contextMenuRenderFunction: renderTableContextMenu,
   });
